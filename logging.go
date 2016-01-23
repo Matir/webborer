7ff5a216d71524726c25ac78ccf2e94f990d1c99
David Tomaschik <david@systemoverlord.com>
2016-01-23 13:37:33 +0800

    Make logging compile with golang < 1.5

--- logging.go
+++ logging.go
@@ -1,11 +1,11 @@
 // Copyright 2015 Google Inc. All Rights Reserved.
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -38,18 +38,12 @@ var logLevelStrings = [...]string{
 }
 
 var logLevel = LogWarning
-
-// Early logging setup.
-// Because we setup logging here, no other init functions should use loggers
-func init() {
-	log.SetOutput(os.Stderr)
-	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
-}
+var defaultLogger = log.New(os.Stderr, "", log.Ldate|log.Ltime|log.Lshortfile)
 
 func ResetLog(settings *ScanSettings) {
 	if len(settings.LogfilePath) > 0 {
 		if fp, err := os.Create(settings.LogfilePath); err == nil {
-			log.SetOutput(fp)
+			defaultLogger = log.New(fp, "", log.Ldate|log.Ltime|log.Lshortfile)
 		} else {
 			Logf(LogError, "Unable to open logfile %s.", settings.LogfilePath)
 		}
@@ -65,7 +59,7 @@ func Logf(level int, format string, args ...interface{}) {
 	}
 	msg := fmt.Sprintf(format, args...)
 	msg = fmt.Sprintf("[%s] %s", logLevelStrings[level], msg)
-	log.Output(2, msg)
+	defaultLogger.Output(2, msg)
 }
 
 func SetLogLevel(level string) {
