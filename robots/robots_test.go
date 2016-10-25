bba2d32b42f1c2239b67e126474a225a074d8895
David Tomaschik <david@systemoverlord.com>
2016-10-24 21:59:11 -0700

    Improve robots coverage.  Refs #14.

--- robots/robots_test.go
+++ robots/robots_test.go
@@ -15,16 +15,20 @@
 package robots
 
 import (
+	"github.com/Matir/gobuster/client/mock"
 	"io/ioutil"
+	"net/http"
+	"net/url"
 	"os"
 	"testing"
 )
 
-func TestParseRobots(t *testing.T) {
+func loadTestRobots(t *testing.T) *RobotsData {
 	testFile, err := os.Open("testdata/test_robots.txt")
 	if err != nil {
 		t.Fatalf("Could not open testdata: %s", err)
 	}
+	defer testFile.Close()
 	buf, err := ioutil.ReadAll(testFile)
 	if err != nil {
 		t.Fatalf("Could not read testdata: %s", err)
@@ -33,6 +37,11 @@ func TestParseRobots(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Could not parse test data: %s", err)
 	}
+	return parsed
+}
+
+func TestParseRobots(t *testing.T) {
+	parsed := loadTestRobots(t)
 
 	expected := &RobotsData{
 		Groups: []RobotsGroup{
@@ -87,6 +96,76 @@ func compareGroup(a, b RobotsGroup, t *testing.T) {
 			if a.Disallow[i] != b.Disallow[i] {
 				t.Errorf("Disallow %s != %s", a.Disallow[i], b.Disallow[i])
 			}
+		}
+	}
+}
+
+func TestGetRobotsForURL(t *testing.T) {
+	testFile, err := os.Open("testdata/test_robots.txt")
+	if err != nil {
+		t.Fatalf("Could not open testdata: %s", err)
+	}
+	defer testFile.Close()
+	resp := &http.Response{
+		StatusCode: 200,
+		Body:       testFile,
+	}
+	factory := &mock.MockClientFactory{
+		ForeverClient: &mock.MockClient{
+			NextResponse: resp,
+		},
+	}
+	data, err := GetRobotsForURL(&url.URL{Scheme: "http", Host: "localhost"}, factory)
+	if err != nil {
+		t.Errorf("Error when loading robots for URL: %v", err)
+	}
+	if data == nil {
+		t.Error("Got nil data when loading robots for URL.")
+	}
+	exp := "http://localhost/robots.txt"
+	if visited := factory.ForeverClient.Requests[0].String(); visited != exp {
+		t.Errorf("Expected to request %s, visited %s", exp, visited)
+	}
+}
+
+func TestGetForUserAgent_A(t *testing.T) {
+	parsed := loadTestRobots(t)
+	paths := parsed.GetForUserAgent("a")
+	for i, p := range []string{"/a", "/b", "/c"} {
+		if paths[i] != p {
+			t.Errorf("Expected path %s, got %s.", p, paths[i])
+		}
+	}
+}
+
+func TestGetForUserAgent_Wildcard(t *testing.T) {
+	parsed := loadTestRobots(t)
+	paths := parsed.GetForUserAgent("wildcard")
+	if len(paths) != 1 {
+		t.Fatalf("Invalid response length: %d", len(paths))
+	}
+	if paths[0] != "/" {
+		t.Errorf("Expected restricting /, got %s.", paths[0])
+	}
+}
+
+func TestGetForUserAgent_Wildcard_Empty(t *testing.T) {
+	rd := &RobotsData{Groups: []RobotsGroup{}}
+	if rv := rd.GetForUserAgent("*"); rv != nil {
+		t.Errorf("Expected nil response for wildcard user-agent, but got %v.", rv)
+	}
+}
+
+func TestGetAllPaths(t *testing.T) {
+	parsed := loadTestRobots(t)
+	expected := []string{"/a", "/b", "/c", "/foo/bar", "/"}
+	results := parsed.GetAllPaths()
+	if le, lr := len(expected), len(results); le != lr {
+		t.Fatalf("Expected and result lengths differ: %d/%d", le, lr)
+	}
+	for i, p := range expected {
+		if p != results[i] {
+			t.Errorf("Expected path %s, got %s", p, results[i])
 		}
 	}
 }
