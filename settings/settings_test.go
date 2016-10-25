2cd98e42a032edb101767b8ec317082719688425
David Tomaschik <david@systemoverlord.com>
2016-10-24 21:16:27 -0700

    Cover custom flags.  #14.

--- settings/settings_test.go
+++ settings/settings_test.go
@@ -17,6 +17,7 @@ package settings
 import (
 	"github.com/Matir/gobuster/logging"
 	"testing"
+	"time"
 )
 
 func TestRobotsModeStrings(t *testing.T) {
@@ -48,5 +49,112 @@ func TestNewScanSettings(t *testing.T) {
 	}
 	if !ss.flagsSet {
 		t.Errorf("Flags not initialized!")
+	}
+}
+
+func TestStringSliceFlag(t *testing.T) {
+	f := StringSliceFlag{}
+	if f.String() != "" {
+		t.Error("Expected empty string for empty StringSliceFlag.")
+	}
+	f.slice = &[]string{}
+	s := "a,b,c"
+	if err := f.Set(s); err != nil {
+		t.Errorf("Error when setting StringSliceFlag: %v", err)
+	}
+	if len(*f.slice) != 3 {
+		t.Errorf("len(f.slice) != 3, = %d", len(*f.slice))
+	}
+	if f.String() != s {
+		t.Errorf("Differing strings: \"%s\" vs \"%s\".", f.String(), s)
+	}
+}
+
+func TestIntSliceFlag(t *testing.T) {
+	f := IntSliceFlag{}
+	if f.String() != "" {
+		t.Error("Expected empty string for empty IntSliceFlag.")
+	}
+	f.slice = &[]int{}
+	s := "1,2,3"
+	if err := f.Set(s); err != nil {
+		t.Errorf("Error when setting IntSliceFlag: %v", err)
+	}
+	if len(*f.slice) != 3 {
+		t.Errorf("len(f.slice) != 3, = %d", len(*f.slice))
+	}
+	if f.String() != s {
+		t.Errorf("Differing strings: \"%s\" vs \"%s\".", f.String(), s)
+	}
+	if err := f.Set("xyz"); err == nil {
+		t.Error("Expected error when setting invalid IntSliceFlag.")
+	}
+}
+
+func TestDurationFlag_Empty(t *testing.T) {
+	f := DurationFlag{}
+	if f.String() != "" {
+		t.Error("Expected empty string for empty DurationFlag.")
+	}
+}
+
+func TestDurationFlag_String(t *testing.T) {
+	d := time.Second
+	f := DurationFlag{&d}
+	if f.String() != "1s" {
+		t.Errorf("Expected \"1s\" for duration: \"%s\"", f.String())
+	}
+}
+
+func TestDurationFlag_Set_Valid(t *testing.T) {
+	d := time.Duration(0)
+	f := DurationFlag{&d}
+	if err := f.Set("1s"); err != nil {
+		t.Errorf("Error setting DurationFlag: %v", err)
+	}
+}
+
+func TestDurationFlag_Set_Invalid(t *testing.T) {
+	d := time.Duration(0)
+	f := DurationFlag{&d}
+	if err := f.Set("blah"); err == nil {
+		t.Error("Expected error setting DurationFlag.")
+	}
+}
+
+func TestRobotsFlag_Empty(t *testing.T) {
+	f := robotsFlag{}
+	if f.String() != "ignore" {
+		t.Errorf("Expected robots flag ignore, got %s.", f.String())
+	}
+}
+
+func TestRobotsFlag_String(t *testing.T) {
+	i := 0
+	f := robotsFlag{&i}
+	if f.String() != "ignore" {
+		t.Errorf("Expected robots flag ignore, got %s.", f.String())
+	}
+}
+
+func TestRobotsFlag_Set_Valid(t *testing.T) {
+	i := 0
+	f := robotsFlag{&i}
+	if err := f.Set("obey"); err != nil {
+		t.Errorf("Expected no error setting robots flag, got %v", err)
+	}
+	if i != ObeyRobots {
+		t.Errorf("Expected flag to be %d, got %d.", ObeyRobots, i)
+	}
+}
+
+func TestRobotsFlag_Set_Invalid(t *testing.T) {
+	i := 0
+	f := robotsFlag{&i}
+	if err := f.Set("wtfmate"); err == nil {
+		t.Error("Expected error setting flag, got nil.")
+	}
+	if i != 0 {
+		t.Errorf("Expected flag unchanged during error, got %d.", i)
 	}
 }
